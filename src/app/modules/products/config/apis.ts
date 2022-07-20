ec63a18ddb132d13e6230b2bb3c2caa8e904a55a
karam  mustafa <karam2mustafa@gmail.com>
2022-07-20 12:44:02 +0300

    wip

---
+++ src/app/modules/products/config/apis.ts
@@ -0,0 +1,3 @@
+export const PRODUCTS_API = {
+  all: 'https://fakestoreapi.com/products'
+};
--- src/app/modules/products/services/products.service.ts
+++ src/app/modules/products/services/products.service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {PRODUCTS_API} from "../config/apis";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor (private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   getAll() {
-    return this.http.get('https://fakestoreapi.com/products');
+    return this.http.get(PRODUCTS_API.all);
   }
 }
